503219e31ac457549fc2ec46566c8e6e4cd5e1e6
Dan Bode <bodepd@gmail.com>
2014-02-19 00:58:21 +0000

    Allow multiple providers for vmware transport
    Currently, the transport resource does not have any
    providers, it is intended as a simple way to specify
    data as resources.
    
    This patch adds basic support for multiple providers so
    that credentials can be passed in ways other than as
    resources.
    
    This is done by calling the getter methods for
    the properties that are used to retrieve credential
    information.
    
    For my use case, it will allow me to store credentials in
    device.conf files (and to use my custom encryption/decryption
    mechanisms.

---
+++ lib/puppet/provider/transport/default.rb
@@ -0,0 +1,23 @@
+Puppet::Type.type(:transport).provide(:default) do
+
+  defaultfor :default_provider => 'true'
+
+  desc 'Basic provider for transport that just returns the value passed into the resource'
+ 
+  def username
+    resource[:username]
+  end
+
+  def password
+    resource[:password]
+  end
+
+  def server
+    resource[:server]
+  end
+
+  def options
+    resource[:options]
+  end
+
+end
