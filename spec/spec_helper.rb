76833107573e22f3a688ba7a07e9ac027bff8946
Nan Liu <nan.liu@gmail.com>
2014-06-06 15:22:04 -0700

    Pin spec_helper gem version.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,5 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
+require 'pathname'
 
-Puppet[:modulepath] = './spec/fixtures/modules'
+path = File.join(Pathname.new(__FILE__).parent, 'fixtures/modules')
+Puppet[:modulepath] = path
